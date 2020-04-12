cfac6061a945b6e0226d52e27c8ad877525f9210
Christoph Buelter <buelter.christoph@gmail.com>
2020-04-12 16:19:09 +0200

    Enable the builtin service worker

--- src/index.js
+++ src/index.js
@@ -158,4 +158,4 @@ app.ports.ask.subscribe(what => {
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();
